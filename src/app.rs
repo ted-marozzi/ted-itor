1618d2efb2cbd40869a999abcba746c7f0ba9198
Ted Marozzi <edwardmarozzi@gmail.com>
2024-01-30 08:26:20 +1100

    s

--- src/app.rs
+++ src/app.rs
@@ -5,8 +5,6 @@ pub fn run_app(app: gpui::App) {
     app.run(|cx: &mut AppContext| {
         Theme::init(cx);
 
-        let _ = cx.observe_keystrokes(|_ev, _cx| println!("App Context keystroke"));
-
         cx.open_window(get_window_options(), build_editor_view);
     });
 }
