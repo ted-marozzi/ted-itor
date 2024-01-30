bdf7ec483365a133dcc680b7387bd5a575f02b12
Ted Marozzi <edwardmarozzi@gmail.com>
2024-01-31 10:22:10 +1100

    Why button no work

--- src/editor.rs
+++ src/editor.rs
@@ -1,46 +1,58 @@
 use gpui::*;
 
-use crate::ui::{Background, Layout};
-
-/* cspell:disable-next-line */
-const INITIAL_EDITOR_TEXT: &str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-
 pub fn build_editor_view(cx: &mut WindowContext<'_>) -> gpui::View<Editor> {
-    let editor_view = cx.new_view(|_cx| Editor {
-        text: INITIAL_EDITOR_TEXT.to_owned(),
-    });
-
-    let keystroke_editor_view = editor_view.clone();
-
-    cx.observe_keystrokes(move |ev, cx| {
-        keystroke_editor_view.update(cx, |editor, cx| {
-            let keystroke = &ev.keystroke.key;
-
-            if let Some(ime_key) = &ev.keystroke.ime_key {
-                editor.text.push_str(ime_key);
-            } else {
-                match keystroke.as_str() {
-                    "backspace" => {
-                        editor.text.pop();
-                    }
-                    keystroke_str => eprintln!("Unhandled keystroke {keystroke_str}"),
-                };
-            }
-
-            cx.notify();
-        });
-    })
-    .detach();
+    let editor_view = cx.new_view(|_cx| Editor {});
 
     editor_view
 }
 
-pub struct Editor {
-    pub text: String,
-}
+pub struct Editor;
 
 impl Render for Editor {
+    fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
+        let view = cx.new_view(|_cx| Counter { count: 1 });
+        let clone = view.clone();
+
+        Button {
+            child: clone.into(),
+            on_click: Box::new(move |_ev, cx| {
+                view.update(cx, |counter, cx: &mut ViewContext<'_, Counter>| {
+                    counter.count += 1;
+                    println!("The count is {}", counter.count); // Always prints 2, no matter how many presses???
+                    cx.notify();
+                });
+            }),
+        }
+    }
+}
+
+#[derive(IntoElement)]
+pub struct Button {
+    child: AnyView,
+    on_click: Box<dyn Fn(&MouseDownEvent, &mut WindowContext)>,
+}
+
+impl RenderOnce for Button {
+    fn render(self, _cx: &mut WindowContext) -> impl IntoElement {
+        div()
+            .flex()
+            .w_full()
+            .h_full()
+            .items_center()
+            .justify_center()
+            .bg(red())
+            .on_mouse_down(MouseButton::Left, self.on_click)
+            .child(self.child)
+    }
+}
+
+pub struct Counter {
+    count: usize,
+}
+
+impl Render for Counter {
     fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
-        Background::new().child(Layout::new().body(self.text.clone()))
+        let current_count = self.count.to_string();
+        div().child(current_count)
     }
 }
